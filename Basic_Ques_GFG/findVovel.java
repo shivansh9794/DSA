9edbd76368365b77176c8667d35610161974bfe7
shivansh9794 <mshivansh549@gmail.com>
2025-03-31 22:35:57 +0530

    Added New QUESTION

---
+++ Basic_Ques_GFG/findVovel.java
@@ -0,0 +1,19 @@
+package Basic_Ques_GFG;
+
+public class findVovel {
+
+    public static void isVowel(char c){
+        if(c=='a'|| c=='A' || c=='e' || c=='E' || c=='i' || c=='I' || c=='o' || c=='O' || c=='u' || c=='U'){
+            System.out.println("Vowel");
+        } 
+        else{
+            System.out.println("Not a Vowel");
+        }
+    } 
+    public static void main(String[] args) {
+        
+        isVowel('a');
+        isVowel('c');
+
+    }
+}
