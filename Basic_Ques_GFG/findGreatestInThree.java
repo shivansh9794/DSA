d422c485f5a2aa09dda12192b16596297f385828
shivansh9794 <mshivansh549@gmail.com>
2025-03-31 18:38:21 +0530

    added new Ques

---
+++ Basic_Ques_GFG/findGreatestInThree.java
@@ -0,0 +1,12 @@
+package Basic_Ques_GFG;
+
+public class findGreatestInThree {
+
+    public static int find_greatest_number(int a, int b, int c){
+        // Write your code here..
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
