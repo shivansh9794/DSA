3f3252ba893be852c4223477c6067b0426adafac
shivansh9794 <mshivansh549@gmail.com>
2025-04-02 21:41:24 +0530

    New Questions

--- Arrays/Array/array1_define.java
+++ Arrays/Array/array1_define.java
@@ -1,16 +1,18 @@
-import java.util.Arrays;
+import java.util.Arrays; 
+import ArrayTraverse;
 
 public class array1_define{
     
     public static void main(String[] args) {
      
-        
+        ArrayTraverse arrt = new ArrayTraverse();
         int intArray[]= new int[3];
 
         intArray[0]=1;
         intArray[1]=2;
         intArray[2]=3;
 
+        arrt.print(intArray);
         System.out.println(Arrays.toString(intArray));
     }
 }
